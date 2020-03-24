a36a089e2d177041fde2769bb79123bb2a907f90
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-03-24 20:36:55 +0900

    Refactor

--- service/src/main/java/com/yamatokataoka/fileserviceapi/LocalFileController.java
+++ service/src/main/java/com/yamatokataoka/fileserviceapi/LocalFileController.java
@@ -1,18 +1,18 @@
 package com.yamatokataoka.fileserviceapi;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.yamatokataoka.fileserviceapi.storage.StorageException;
 import com.yamatokataoka.fileserviceapi.storage.StorageService;
