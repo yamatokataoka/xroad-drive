cd1ed7699b0f485b2b31948a79107a9884b41b43
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-04-12 16:01:07 +0900

    Add FileServiceTest

---
+++ api/src/test/java/com/yamatokataoka/fileservice/api/service/FileServiceTest.java
@@ -0,0 +1,41 @@
+package com.yamatokataoka.fileservice.api.service;
+
+import com.yamatokataoka.fileservice.api.service.FileService;
+import com.yamatokataoka.fileservice.api.service.StorageService;
+import com.yamatokataoka.fileservice.api.repository.FileRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class FileServiceTest {
+
+  @Mock
+  private StorageService storageService;
+
+  @Mock
+  private FileRepository fileRepository;
+
+  private FileService fileservice;
+
+  @BeforeEach
+  void setup() {
+    fileservice = new FileService(storageService, fileRepository);
+  }
+
+  @Test
+  void testStoreSuccess() {
+    MockMultipartFile mockMultipartFile = new MockMultipartFile("name", "originalFilename.txt", "text/plain", "some text".getBytes());
+    fileservice.store(mockMultipartFile);
+
+    verify(storageService, times(1)).store(any(), any());
+    verify(fileRepository, times(1)).save(any());
+  }
+}
