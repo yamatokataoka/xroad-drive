a6bac254ddae3fa3cc493d828566ad4efdf26de8
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-04-21 19:23:34 +0900

    Add tool bar

---
+++ fileservice-ui/src/components/ToolBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-toolbar
+    app
+    dense
+  >
+    <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
+    <v-spacer />
+    <v-btn icon>
+      <v-icon>mdi-trash-can-outline</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-download-outline</v-icon>
+    </v-btn>
+    <v-divider
+      class="mx-4"
+      vertical
+    ></v-divider>
+    <v-btn icon>
+      <v-icon>mdi-file-upload-outline</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'ToolBar',
+  data: () => ({
+    pageTitle: 'MyFiles',
+  }),
+};
+</script>
+
+<style scoped>
+</style>
