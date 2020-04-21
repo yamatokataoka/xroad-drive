776ec0e56fcb2518020a0beea5668e4576909cf5
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-04-21 19:23:12 +0900

    Add file list

---
+++ fileservice-ui/src/components/FileList.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="fileList"
+    hide-default-footer
+    fixed-header
+    single-select
+    @click:row="rowClick"
+    class="elevation-1"
+  >
+    <template v-slot:no-data>No Files</template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'FileList',
+  data: () => ({
+    selectedId: -1,
+    fileList: [
+      {
+        id: 1,
+        name: 'file_a.txt',
+        owner: 'test_user',
+        lastModifiedDate: '09.05.2020',
+        size: '100 MB'
+      }, {
+        id: 2,
+        name: 'file_b.txt',
+        owner: 'test_user',
+        lastModifiedDate: '09.06.2020',
+        size: '150 KB'
+      }, {
+        id: 3,
+        name: 'file_c.txt',
+        owner: 'test_user',
+        lastModifiedDate: '09.07.2020',
+        size: '250 KB'
+      }
+    ],
+    headers: [
+      { text: 'Name', value: 'name' },
+      { text: 'Owner', value: 'owner' },
+      { text: 'Last modified', value: 'lastModifiedDate' },
+      { text: 'Size', value: 'size' },
+    ],
+  }),
+  methods: {
+    rowClick: function (item, row) {      
+      row.select(true);
+      this.selectedId=item.name
+    }
+  },
+};
+</script>
+<style scoped>
+</style>
