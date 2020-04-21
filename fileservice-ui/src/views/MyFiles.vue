fa404c4dc71cae220c4a4593dd26e83406000281
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-04-21 19:24:18 +0900

    Add FileList and ToolBar on view

--- fileservice-ui/src/views/MyFiles.vue
+++ fileservice-ui/src/views/MyFiles.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>My Files</h1>
+  <div>
+    <tool-bar />
+    <file-list />
+  </div>
 </template>
 
 <script>
+import FileList from '@/components/FileList';
+import ToolBar from '@/components/ToolBar';
 
 export default {
-  name: 'MyFiles'
+  name: 'MyFiles',
+  components: { FileList, ToolBar },
 }
 </script>
