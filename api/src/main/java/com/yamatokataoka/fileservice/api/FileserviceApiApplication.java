4df0c2814b193027128bb918db78d2c1aa3f4c2a
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-04-24 20:49:34 +0900

    Remove init

--- api/src/main/java/com/yamatokataoka/fileservice/api/FileserviceApiApplication.java
+++ api/src/main/java/com/yamatokataoka/fileservice/api/FileserviceApiApplication.java
@@ -1,12 +1,8 @@
 package com.yamatokataoka.fileservice.api;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import com.yamatokataoka.fileservice.api.service.StorageService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -14,12 +10,5 @@ public class FileserviceApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileserviceApiApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.init();
-		};
 	}
 }
