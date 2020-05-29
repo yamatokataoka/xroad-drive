b030f266dcd6421c6502b6012dce8ab56fcc2a26
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-05-29 13:40:42 +0900

    Add file list view for shared with others

---
+++ xroad-drive-ui/src/components/fileView/FileListSharedWithOthers.vue
@@ -0,0 +1,103 @@
+<template>
+  <!-- todo: hard-coded height -->
+  <v-data-table
+    :headers="headers"
+    :items="fileListWithCreatedDate"
+    group-by="createdDate"
+    hide-default-footer
+    fixed-header
+    single-select
+    @click:row="clickRow"
+    height="calc(100vh - 96px)"
+    item-key="id"
+  >
+    <template v-slot:no-data>No Files</template>
+    <template v-slot:item.filename="{ item }">
+      <v-icon class="me-4">mdi-file-outline</v-icon>
+      <span>{{ item.filename }}</span>
+    </template>
+    <template v-slot:item.createdDateTime="{ item }">
+      <span v-if="isToday(item.createdDateTime)">today {{ item.createdDateTime | formatHoursMins }}</span>
+      <span v-else>{{ item.createdDateTime | formatDate }}</span>
+    </template>
+    <template v-slot:item.filesize="{ item }">
+      <span>{{ item.filesize | formatBytes }}</span>
+    </template>
+    <template v-slot:group.header="{ group, headers }">
+      <th :colspan="headers.length">
+        {{ group | formatDate }}
+      </th>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+  import { mapState, mapActions } from 'vuex';
+
+  export default {
+    name: 'FileListSharedWithOthers',
+    props: {
+      selectedFile: {
+        required: true
+      }
+    },
+    data() {
+      return {
+        headers: [
+          { text: 'Name', value: 'filename' },
+          { text: 'Shared At', value: 'createdDateTime' },
+          { text: 'Size', value: 'filesize' }
+        ]
+      }
+    },
+    computed: {
+      ...mapState('fileList', ['fileList']),
+      fileListWithCreatedDate: function() {
+        return this.fileList.map(item => ({
+          ...item, createdDate: this.$options.filters.formatDate(item.createdDateTime)
+        }));
+      }
+    },
+    methods: {
+      ...mapActions('selectedFile', ['updateSelectedFile']),
+      ...mapActions('fileList', ['fetchFileList']),
+      clickRow(item, row) {
+        if (this.selectedFile && row.isSelected) {
+          row.select(false);
+          this.updateSelectedFile(null);
+        } else {
+          row.select(true);
+          this.updateSelectedFile(item);
+        }
+      },
+      pollFileList() {
+        this.fetchFileList();
+
+        this.polling = setInterval(() => {
+          this.fetchFileList();
+        }, 3000)
+      },
+      isToday(dateString) {
+        const inputDate = this.$options.filters.formatDate(dateString);
+        const today = new Date().toDateString();
+        return (today == inputDate);
+      }
+    },
+    created() {
+      this.pollFileList()
+    },
+    beforeRouteLeave() {
+      clearInterval(this.polling)
+    }
+  };
+</script>
+
+<style scoped>
+  /* todo: hard-coded fixes */
+  >>>.v-row-group__header {
+    background: unset!important;
+  }
+  >>>td:not(.v-data-table__mobile-row) {
+    border-bottom: unset!important;
+  }
+</style>
--- xroad-drive-ui/src/views/SharedWithOthers.vue
+++ xroad-drive-ui/src/views/SharedWithOthers.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1>Shared with others</h1>
+  <div>
+    <toolbar
+      :pageTitle="pageTitle"
+      :selectedFile="selectedFile"
+    ></toolbar>
+    <file-list-shared-with-others
+      :selectedFile="selectedFile"
+    ></file-list-shared-with-others>
+    <file-upload-dialog />
+  </div>
 </template>
+
+<script>
+  import { mapState, mapActions } from 'vuex';
+  import FileListSharedWithOthers from '@/components/fileView/FileListSharedWithOthers';
+  import Toolbar from '@/components/fileView/Toolbar';
+  import FileUploadDialog from '@/components/ui/FileUploadDialog';
+
+  export default {
+    name: 'SharedWithOthers',
+    components: {
+      FileListSharedWithOthers,
+      Toolbar,
+      FileUploadDialog
+    },
+    data() {
+      return {
+        pageTitle: 'Shared With Others'
+      }
+    },
+    computed: {
+      ...mapState('selectedFile', ['selectedFile'])
+    },
+    methods: {
+      ...mapActions('selectedFile', ['updateSelectedFile'])
+    },
+    beforeRouteLeave(to, from, next) {
+      this.updateSelectedFile(null);
+      next();
+    }
+  };
+</script>
