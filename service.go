42e0b50e614a84f0ff976061025ebe134cc160c6
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-08-10 05:44:43 +0000

    Add unit test for client service

--- service.go
+++ service.go
@@ -1,15 +1,33 @@
 package proxy
 
+import "errors"
+
+type XRoadMemberService interface {
+  GetAll() ([]*XRoadMember, error)
+}
+
 type ProviderService interface {
-	GetAll() ([]*XRoadMember, error)
+	XRoadMemberService
+}
+
+type ClientService interface {
+	XRoadMemberService
 }
 
 type providerService struct {
 	pr ProviderRepository
 }
 
+type clientService struct {
+	cr ClientRepository
+}
+
 func NewProviderService(pr ProviderRepository) ProviderService {
   return &providerService{pr}
+}
+
+func NewClientService(cr ClientRepository) ClientService {
+  return &clientService{cr}
 }
 
 func (ps *providerService) GetAll() ([]*XRoadMember, error) {
@@ -18,4 +36,8 @@ func (ps *providerService) GetAll() ([]*XRoadMember, error) {
     return nil, err
   }
   return providers, nil
+}
+
+func (cs *clientService) GetAll() ([]*XRoadMember, error) {
+  return nil, errors.New("Not implemented")
 }
