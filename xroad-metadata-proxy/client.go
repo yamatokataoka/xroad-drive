451b83c97f2b67b0725e2ab94a1e2e1bf9c0af63
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-08-10 11:41:14 +0000

    Refactor log

--- xroad-metadata-proxy/client.go
+++ xroad-metadata-proxy/client.go
@@ -14,11 +14,16 @@ func NewClient() *redis.Client {
     DB:       0,
   })
 
+  // TODO: handle and log in the case, connected redis -> disconnected
   go retry(10*time.Second, func() (err error) {
     _, err = client.Ping().Result()
-    log.
+    if err != nil {
+      log.
       WithError(err).
       Error("Failed to connect redis")
+    } else {
+      log.Info("Connected to redis")
+    }
     return err
   })
 
