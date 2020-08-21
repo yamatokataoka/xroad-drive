e1923e1c72f83dbcdb4636afbef6b83d851af702
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-08-21 22:59:44 +0000

    Refactor
    no need changeOrigin param here.

--- xroad-drive-ui/vue.config.js
+++ xroad-drive-ui/vue.config.js
@@ -7,8 +7,7 @@ module.exports = {
     https: false,
     proxy: {
       '^/api': {
-        target: process.env.VUE_APP_API_URL || 'http://localhost:8082',
-        changeOrigin: true
+        target: process.env.VUE_APP_API_URL || 'http://localhost:8082'
       }
     }
   }
