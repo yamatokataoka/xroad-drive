4f00925bfb997952569ad8ff8651124f82ea32cf
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-08-23 08:42:52 +0000

    Refactor
    Delete sample data.
    Use variable instead of hard code.

--- xroad-drive-ui/src/store/modules/xroadMetadata.js
+++ xroad-drive-ui/src/store/modules/xroadMetadata.js
@@ -4,30 +4,8 @@ export default {
   namespaced: true,
   state() {
     return {
-      providers: [
-        {
-          id: 'CS:ORG:1111:Company1Provider',
-          name: 'Company 1'
-        }, {
-          id: 'CS:ORG:1112:Company2Provider',
-          name: 'Company 2'
-        }, {
-          id: 'CS:ORG:1113:Company3Provider',
-          name: 'Company 3'
-        }
-      ],
-      clients: [
-        {
-          id: 'CS:ORG:1111:Company1Provider',
-          name: 'Company 1'
-        }, {
-          id: 'CS:ORG:1114:Company4Provider',
-          name: 'Company 4'
-        }, {
-          id: 'CS:ORG:1115:Company5Provider',
-          name: 'Company 5'
-        }
-      ]
+      providers: [],
+      clients: []
     };
   },
   getters: {
@@ -36,7 +14,7 @@ export default {
         let providersForNav = [];
         for (let i = 0; i < state.providers.length; i++) {
           providersForNav[i] = {...state.providers[i]};
-          providersForNav[i].id = 'shared-with-us' + ':' + state.providers[i].id;
+          providersForNav[i].id = parentName + ':' + state.providers[i].id;
           providersForNav[i].to = { name: parentName, params: { id: state.providers[i].id }};
           providersForNav[i].parent = parentName;
         }
@@ -48,7 +26,7 @@ export default {
         let clientsForNav = [];
         for (let i = 0; i < state.clients.length; i++) {
           clientsForNav[i] = {...state.clients[i]};
-          clientsForNav[i].id = 'shared-with-others' + ':' + state.clients[i].id;
+          clientsForNav[i].id = parentName + ':' + state.clients[i].id;
           clientsForNav[i].to = { name: parentName, params: { id: state.clients[i].id }};
           clientsForNav[i].parent = parentName;
         }
