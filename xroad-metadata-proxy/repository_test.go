5d8af474added866c898c0796b51e58275783539
Yamato Kataoka <yk_time@icloud.com>
2020-09-05 13:53:02 +0900

    Add shared test data to make it DRY

--- xroad-metadata-proxy/repository_test.go
+++ xroad-metadata-proxy/repository_test.go
@@ -6,45 +6,40 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
+var mapXRoadMembers = []map[string]interface{}{
+  {"ID": "CS:ORG:1111:Company1Subsystem", "Name": "Company 1"},
+  {"ID": "CS:ORG:1112:Company2Subsystem", "Name": "Company 2"},
+  {"ID": "CS:ORG:1113:Company3Subsystem", "Name": "Company 3"},
+}
+
 func TestProviderRepository_GetAll(t *testing.T) {
   mockClient, miniredis := NewRedisMock(t)
   defer miniredis.Close()
 
-  mapProviders := []map[string]interface{}{
-    {"ID": "CS:ORG:1111:Company1Provider", "Name": "Company 1"},
-    {"ID": "CS:ORG:1112:Company2Provider", "Name": "Company 2"},
-    {"ID": "CS:ORG:1113:Company3Provider", "Name": "Company 3"},
-  }
-
-  HMSetAll(t, mapProviders, mockClient, providerKey)
-  expectedProviders := decodeXRoadMembers(t, mapProviders)
+  HMSetAll(t, mapXRoadMembers, mockClient, providerKey)
+  expectedXRoadMembers := decodeXRoadMembers(t, mapXRoadMembers)
 
   providerRepository := NewProviderRepository(mockClient)
-  actualProviders, err := providerRepository.GetAll()
+  actualXRoadMembers, err := providerRepository.GetAll()
   if err != nil {
     t.Errorf("Failed to call GetAll '%#v'", err)
   }
 
-  assert.Equal(t, expectedProviders, actualProviders)
+  assert.Equal(t, expectedXRoadMembers, actualXRoadMembers)
 }
 
 func TestClientRepository_GetAll(t *testing.T) {
   mockClient, miniredis := NewRedisMock(t)
   defer miniredis.Close()
 
-  mapClients := []map[string]interface{}{
-    {"ID": "CS:ORG:1111:Company1Provider", "Name": "Company 1"},
-    {"ID": "CS:ORG:1112:Company2Provider", "Name": "Company 2"},
-  }
-
-  HMSetAll(t, mapClients, mockClient, clientKey)
-  expectedClients := decodeXRoadMembers(t, mapClients)
+  HMSetAll(t, mapXRoadMembers, mockClient, clientKey)
+  expectedXRoadMembers := decodeXRoadMembers(t, mapXRoadMembers)
 
   clientRepository := NewClientRepository(mockClient)
-  actualClients, err := clientRepository.GetAll()
+  actualXRoadMembers, err := clientRepository.GetAll()
   if err != nil {
     t.Errorf("Failed to call GetAll '%#v'", err)
   }
 
-  assert.Equal(t, expectedClients, actualClients)
+  assert.Equal(t, expectedXRoadMembers, actualXRoadMembers)
 }
